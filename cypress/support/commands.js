b3aef7d570ed81c0bfc4b6fa055c3dfe2047e3d5
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-19 17:15:48 -0300

    Implementação de comandos customizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,52 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('registerNewUser', (email, password) => {
+    cy.get('#reg_email')
+        .type(email)
+    cy.get('#reg_password')
+        .type(`${password}{enter}`)
+})
 
-
-Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('#username')
+        .type(email)
+    cy.get('#password')
+        .type(`${password}{enter}`, { log: false })
 });
 
+Cypress.Commands.add('addProductToCart', (productName, size, color, quantity) => {
+    cy.get('[class = "product-block grid"]')
+        .contains(productName)
+        .click()
+    cy.get('.button-variable-item-' + size)
+        .click()
+    cy.get('.button-variable-item-' + color)
+        .click()
+    cy.get('.input-text')
+        .clear()
+        .type(quantity)
+    cy.get('.single_add_to_cart_button')
+        .click()
+    cy.get('#primary-menu > .menu-item-629 > a')
+        .click()
+});
+
+Cypress.Commands.add('checkoutFormFill', (name, lastName, country, address, city, state, zipCode, telephone) => {
+    cy.get('#billing_first_name')
+        .type(name)
+    cy.get('#billing_last_name')
+        .type(lastName)
+    cy.get('#select2-billing_country-container')
+        .click()
+    cy.get('.select2-search__field')
+        .type(`${country}{enter}`)
+    cy.get('#billing_address_1')
+        .type(address)
+    cy.get('#billing_city')
+        .type(city)
+    cy.get('#select2-billing_state-container')
+        .click()
+        .type(`${state}{enter}`)
+    cy.get('#billing_postcode')
+        .type(zipCode)
+    cy.get('#billing_phone')
+        .type(telephone)
+})
